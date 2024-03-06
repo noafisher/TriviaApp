d059eb5266f3e2db8c373c0e466dab23f859e606
User <User@LAB3-3>
2024-03-06 09:16:26 +0200

    added alot

--- Services/TriviaAppService.cs
+++ Services/TriviaAppService.cs
@@ -10,14 +10,20 @@ namespace TriviaApp.Services
     public class TriviaAppService
     {
         List<User> users;
-
+        List<Question> questions;
+        List<StatusQuestion> statusQuestions;
 
         public TriviaAppService()
         {
             users = new List<User>();
             users.Add(new User() { UserName = "NoaF", Password = "110907n", Points = 1000, Rank = new Rank{ RankId = 1, RankName = "Manager" } , Email="noa.fisher.2007@gmail.com" });
             users.Add(new User() { UserName = "Shahar", Password = "290807s", Points=500 ,Rank = new Rank { RankId = 2, RankName = "Master" }, Email="shahr.oz298@gmail.com" });
-
+            statusQuestions = new List<StatusQuestion>();
+            statusQuestions.Add(new StatusQuestion() { StatusId = 1, StatusDescription = "pending" });
+            statusQuestions.Add(new StatusQuestion() { StatusId = 2, StatusDescription = "approved" });
+            statusQuestions.Add(new StatusQuestion() { StatusId = 3, StatusDescription = "declined" });
+            questions = new List<Question>();
+            questions.Add(new Question() { QuestionId = 1, Subject = new SubjectQuestion() { SubjectId = 1, SubjectName = "Animals" }, CreatedBy = "NoaF", Status = statusQuestions.Where(x=>x.StatusId==1).FirstOrDefault(), Text="how many arms does an octopus have?", Ranswer="8", Wanswer1="10", Wanswer2="4", Wanswer3="30" });
         }
 
         public bool Login(string name, string pass)
@@ -30,7 +36,14 @@ namespace TriviaApp.Services
             return this.users.OrderByDescending(u => u.Points).ToList();
         }
 
-       
+       public List<Question> GetPendingQuestions()
+        {
+            return questions.Where(x=>x.Status.StatusId == 1).ToList();
+        }
+        public void ApproveQuestion(Question q)
+        {
+            q.Status = statusQuestions.Where(x => x.StatusId == 2).FirstOrDefault();
+        }
 
     }
 }
